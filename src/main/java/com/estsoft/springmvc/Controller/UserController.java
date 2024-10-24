ac247be893a728eb96135f193edf6568adb20d80
JSW0721 <stmjdi04@gmail.com>
2024-10-24 15:41:29 +0900

    서버 AWS에 등록

--- src/main/java/com/estsoft/springmvc/Controller/UserController.java
+++ src/main/java/com/estsoft/springmvc/Controller/UserController.java
@@ -2,9 +2,11 @@ package com.estsoft.springmvc.Controller;
 
 import com.estsoft.springmvc.DTO.AddUserRequest;
 import com.estsoft.springmvc.Service.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
     private final UserService userService;
 
